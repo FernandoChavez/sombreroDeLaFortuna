369d50751822d7edad6bd7bbc185e03a00d386d4
FuerzaDON <luis_fernando_chavez@hotmail.com>
2020-10-13 11:32:00 -0500

    react-bootstrap removido y sombras agregado a la tarjeta ganadora

--- src/components/Ganadores.js
+++ src/components/Ganadores.js
@@ -8,15 +8,15 @@ const Ganadores = ({ganadores}) =>{
     if(!ganadores) return null;
 
     return (
-        <div className="card mb-3">
+        <div className="card mb-3 carta-ganadora">
             <div className="row no-gutters">
                 <div className="col-md-4">
                     <img className="card-img" src={prize} alt="steve"/> 
                 </div>
                 <div className="col-md-8">
                 <div className="card-body">
-                    <h5 className="card-title">FuerzaDON de la fortuna</h5>
-                    <p className="card-text">El ganador del DLC de Steve en SSBU es: <b>{ganadores}</b></p>
+                    <h5 className="card-title">La fortuna de FuerzaDON ha hablado</h5>
+                    <p className="card-text">El ganador del DLC de Steve es: <b>{ganadores}</b></p>
                 </div>
                 </div>
             </div>
