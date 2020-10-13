a25646559c0603e2baa1d263de2993a059d5fada
FuerzaDON <luis_fernando_chavez@hotmail.com>
2020-10-13 11:52:51 -0500

    referencia a react bootstrap removido

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Header from './components/Header';
 import Formulario from './components/Formulario';
